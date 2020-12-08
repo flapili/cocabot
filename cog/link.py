69b190682e2d6a50ca516d75c35b69b7e0e807b1
Benoit DEVEAUX <contact.flapili@gmail.com>
2020-12-08 12:25:37 +0100

    reject some website and images

--- cog/link.py
+++ cog/link.py
@@ -1,7 +1,10 @@
 import logging
+import ssl
+import itertools
 
 import discord
 from discord.ext import commands
+import aiohttp
 
 from utils import regex
 
@@ -23,16 +26,41 @@ class Link(commands.Cog):
             532970830246707251,  # général
             548982055782973461,  # astuce
         )
+
+        rejected_website = ("tenor.com",)
+        rejected_url = tuple(
+            f"{scheme}://{link}"
+            for link, scheme in itertools.product(rejected_website, ("http", "https"))
+        )
+
         if message.channel.id in allowed_channels_id:
             channel = self.bot.get_channel(742516338857213995)  # liens stylés
             if channel is None:
                 logger.error("channel 'Liens Stylés' not found !")
                 return
 
-            if regex.url_regex.findall(message.content):
+            for url in regex.url_regex.findall(message.content):
+                print(url[0])
+                if url[0].startswith(rejected_url):
+                    continue
+
+                try:
+                    async with aiohttp.ClientSession() as ses:
+                        async with ses.get(url[0]) as resp:
+                            if resp.status != 200:
+                                continue
+                            if resp.content_type.startswith("image/"):
+                                continue
+                except ssl.SSLError:
+                    continue
+
                 embed = discord.Embed()
-                embed.add_field(name="author", value=message.author.mention, inline=False)
-                embed.add_field(name="jump", value=f"[jump]({message.jump_url})", inline=False)
+                embed.add_field(
+                    name="author", value=message.author.mention, inline=False
+                )
+                embed.add_field(
+                    name="jump", value=f"[jump]({message.jump_url})", inline=False
+                )
                 await channel.send(message.content)
                 await channel.send(embed=embed)
 
