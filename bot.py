9a2063fc68b2762f87b51ff177c867b003627f42
Benoit DEVEAUX <contact.flapili@gmail.com>
2021-05-02 14:02:37 +0200

    fix extension loader (du to 3.9 __file__ breacking change)

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ from discord.ext import commands
 
 from utils.my_bot import MyBot
 
+os.chdir(Path(__file__).parent)
+
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.DEBUG)
 
@@ -43,16 +45,15 @@ async def on_ready():
     logger.info("The bot is ready")
 
 
-for file in Path(__file__).parent.glob("cog/*.py"):
-    if file.stem != "__init__":
-        extension = ".".join(file.parts[:-1] + (file.stem,))
-        try:
-            bot.load_extension(extension)
-        except Exception:
-            logger.error(f"Error while loading extension {extension}")
-            logger.error(traceback.format_exc())
-        else:
-            logger.info(f"Loaded {extension}")
+for file in Path().parent.glob("cog/**/*.py"):
+    extension = ".".join(file.parts[:-1] + (file.stem,))
+    try:
+        bot.load_extension(extension)
+    except Exception:
+        logger.error(f"Error while loading extension {extension}")
+        logger.error(traceback.format_exc())
+    else:
+        logger.info(f"Loaded {extension}")
 
 
 bot.run(os.environ["TOKEN"])
