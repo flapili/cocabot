ae9d5f5772a4776b537ce1c5c5281d78a8ffb434
Benoit DEVEAUX <contact.flapili@gmail.com>
2021-05-02 16:24:30 +0200

    add typehint, change send to reply

--- cog/error_handler.py
+++ cog/error_handler.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from typing import Type
 import traceback
 import logging
 import io
@@ -6,6 +7,7 @@ import io
 import discord
 from discord.ext import commands
 
+from utils.my_bot import MyBot
 
 logger = logging.getLogger(__file__)
 
@@ -15,11 +17,13 @@ class ErrorHandler(commands.Cog):
     Error handler.
     """
 
-    def __init__(self, bot):
+    def __init__(self, bot: MyBot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
+    async def on_command_error(
+        self, ctx: commands.Context, error: Type[commands.errors.CommandError]
+    ):
 
         if isinstance(error, commands.errors.CheckAnyFailure):
             error = error.errors[0]
@@ -32,7 +36,7 @@ class ErrorHandler(commands.Cog):
                 commands.errors.NotOwner,
             ),
         ):
-            await ctx.send(
+            await ctx.reply(
                 f"Tu n'as pas les droits d'utiliser `{ctx.command}` {ctx.author.mention} !"
             )
             return
@@ -40,29 +44,34 @@ class ErrorHandler(commands.Cog):
         if isinstance(error, commands.errors.CommandOnCooldown):
             if ctx.author.guild_permissions.administrator:
                 await ctx.reinvoke()
-            else:
-                minutes, seconds = divmod(int(error.retry_after), 60)
-                hours, minutes = divmod(minutes, 60)
-                days, hours = divmod(hours, 24)
-                weeks, days = divmod(days, 7)
-                units = [
-                    (weeks, "semaine(s)"),
-                    (days, "jour(s)"),
-                    (hours, "heure(s)"),
-                    (minutes, "minute(s)"),
-                    (seconds, "seconde(s)"),
-                ]
+                return
 
-                cd = [f"{time} {unit}" for time, unit in units if time]
-                cd = (", ".join(cd[:-1]) + " et " + cd[-1]) if len(cd) > 1 else cd[0]
-                await ctx.send(
-                    f"{ctx.author.mention} la commande `{ctx.command.qualified_name}`"
-                    f" est en cooldown, merci d'attendre {cd}"
-                )
+            if await self.bot.is_owner(ctx.author):
+                await ctx.reinvoke()
+                return
+
+            minutes, seconds = divmod(int(error.retry_after), 60)
+            hours, minutes = divmod(minutes, 60)
+            days, hours = divmod(hours, 24)
+            weeks, days = divmod(days, 7)
+            units = [
+                (weeks, "semaine(s)"),
+                (days, "jour(s)"),
+                (hours, "heure(s)"),
+                (minutes, "minute(s)"),
+                (seconds, "seconde(s)"),
+            ]
+
+            cd = [f"{time} {unit}" for time, unit in units if time]
+            cd = (", ".join(cd[:-1]) + " et " + cd[-1]) if len(cd) > 1 else cd[0]
+            await ctx.reply(
+                f"{ctx.author.mention} la commande `{ctx.command.qualified_name}`"
+                f" est en cooldown, merci d'attendre {cd}"
+            )
             return
 
         if isinstance(error, commands.errors.MissingRequiredArgument):
-            await ctx.send(f"Argument manquant : {error.param.name}")
+            await ctx.reply(f"Argument manquant : {error.param.name}")
             await ctx.send_help(ctx.command)
             return
 
@@ -72,7 +81,7 @@ class ErrorHandler(commands.Cog):
             return
 
         if isinstance(error, commands.errors.DisabledCommand):
-            await ctx.send(
+            await ctx.reply(
                 f"la commande `{ctx.prefix}{ctx.command}` est désactivée pour la raison suivante :"
                 f" {getattr(ctx.command, '__original_kwargs__').get('reason', None)}"
             )
@@ -82,10 +91,6 @@ class ErrorHandler(commands.Cog):
             error, (commands.errors.CommandNotFound, commands.errors.CheckFailure)
         ):
             return
-
-        crash_log = "".join(
-            traceback.format_exception(type(error), error, error.__traceback__)
-        )
 
         embed = discord.Embed()
         embed.add_field(
@@ -100,6 +105,9 @@ class ErrorHandler(commands.Cog):
         embed.add_field(name="author id", value=ctx.author.id, inline=False)
         embed.add_field(name="message", value=ctx.message.content[:1024], inline=False)
 
+        crash_log = "".join(
+            traceback.format_exception(type(error), error, error.__traceback__)
+        )
         file = discord.File(io.StringIO(crash_log), filename="crash_log.txt")
         try:
             await ctx.send(embed=embed, file=file)
